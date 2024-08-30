c085b8e3f1a5708e91727af056867118037bbb8d
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-08-30 15:59:41 -0300

    commit_creatPass_CreateUser_inDB

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const app = express();
 //--Config JSON response
 app.use(express.json());
 
+//--Models--
+const User = require('./models/User');
+
 //--Open Route - Public Route--
 app.get('/', (req, res) => {
     res.status(200).json({msg: 'Bem vindo a nossa API!'});
@@ -23,6 +26,48 @@ app.post('/auth/register', async(req, res) => {
     //validations
     if(!name){
         return res.status(422).json({ msg: 'O nome é obrigatorio!'});
+    }
+
+    if(!email){
+        return res.status(422).json({ msg: 'O email é obrigatorio!'});
+    }
+
+    if(!password){
+        return res.status(422).json({ msg: 'A senha é obrigatoria!'});
+    }
+
+    if(password !== confirmpassword){
+        return res.status(422).json({ msg: 'As senhas não conferem!'});
+    }
+
+    //--Check if user exists--
+    const userExists = await User.findOne({ email: email });
+
+    if(userExists){
+        return res.status(422).json({ msg: 'Por favor, utilize outro e-mail!'});
+    }
+
+    //--Create password--
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    //--Create user--
+    const user = new User ({
+        name,
+        email,
+        password: passwordHash,
+    });
+
+    try{
+
+        await user.save();
+
+        res.status(201).json({ msg: 'Usuário criado com sucesso!'});
+
+    } catch(error) {
+        console.log(error);
+
+        res.status(500).json({ msg: 'Erro no servidor, tente novamente mais tarde!'});
     }
 
 });
