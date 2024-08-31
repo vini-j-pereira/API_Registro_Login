6ce6c254d25b78f87d0c4e69b6d81aaa15dc016e
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-08-30 21:12:45 -0300

    commit_API

--- app.js
+++ app.js
@@ -18,6 +18,44 @@ app.get('/', (req, res) => {
     res.status(200).json({msg: 'Bem vindo a nossa API!'});
 });
 
+//--Private Route--
+app.get("/user/:id", checkToken, async (req, res) => {
+
+    const id = req.params.id;
+
+    //check if user exists
+    const user = await User.findById(id, '-password');
+
+    if(!user){
+        return res.status(404).json({ msg: 'Usuário não encontrado!'});
+    }
+
+    res.status(200).json({ user });
+
+});
+
+function checkToken (req, res, next) {
+
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if(!token){
+        return res.status(401).json({ msg: 'Acesso negado!'});
+    }
+
+    try {
+
+        const secret = process.env.SECRET;
+
+        jwt.verify(token, secret);
+
+        next();
+
+    } catch(error) {
+        res.status(400).json({ msg: 'Token inválido'});
+    }
+}
+
 //--Register-User--
 app.post('/auth/register', async(req, res) => {
 
@@ -68,6 +106,56 @@ app.post('/auth/register', async(req, res) => {
         console.log(error);
 
         res.status(500).json({ msg: 'Erro no servidor, tente novamente mais tarde!'});
+    }
+
+});
+
+//--Login User--
+app.post("/auth/login", async (req, res) => {
+
+    const {email, password} = req.body;
+
+    //validations
+    if(!email){
+        return res.status(422).json({ msg: 'O email é obrigatorio!'});
+    }
+
+    if(!password){
+        return res.status(422).json({ msg: 'A senha é obrigatoria!'});
+    }
+
+    //--Check if user exists--
+    const user = await User.findOne({ email: email});
+
+    if(!user){
+        return res.status(404).json({ msg: 'Usuário não encontrado!'});
+    }
+
+    //--Check if password match--
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if(!checkPassword){
+        return res.status(422).json({ msg: 'Senha inválida!'});
+    }
+
+    try{
+
+        const secret = process.env.SECRET;
+
+        const token = jwt.sign({
+            id: user._id,
+        },
+        secret,
+    )
+
+    res.status(200).json({ msg: 'Autenticação realizada com sucesso!', token });
+
+    } catch (err) {
+        console.log(error);
+
+        res.status(500).json({
+            msg: 'Aconteceu um erro no servidor, tente novamente mais tarde!',
+        });
     }
 
 });
